249d7382436996677454b9dadac01a1b63198dc8
ofenbach <t.ofenbach@web.de>
2021-02-21 21:45:10 +0100

    Add files via upload

--- server/channel.py
+++ server/channel.py
@@ -8,7 +8,7 @@ class Channel:
 
     def __init__(self, name, port):
         """ Creates new channel with individual port """
-        print(Channel.ip)
+
         # define id
         self.name = name
         self.port = port
@@ -22,10 +22,13 @@ class Channel:
 
     def accept_user_joins(self):
         """ Always checks if user wants to connect """
+
+        # setup socket
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.bind((Channel.ip, self.port))
         self.s.listen()
 
+        # listen if users connect
         while True:
             conn, addr = self.s.accept()
             self.users.append(conn)
@@ -34,32 +37,24 @@ class Channel:
 
     def receive_audio_from_user(self, conn, addr):
         """ Receives from every user audio """
+
         while 1:
             try:
-                data = conn.recv(self.chunk_size)
-                self.send_audio_to_users(conn, data)
+                data = conn.recv(self.chunk_size)       # receive audio from user conn
+                self.send_audio_to_users(conn, data)    # then send it to everyone else
             except socket.error:
                 self.users.remove(conn)
                 print("User left channel: " + self.name +" "+ str(len(self.users)) + "/10") # TODO: Fix: Server crashes
-                conn.close()
+                conn.close()    # TODO: Socket reconnect bugs
                 break
 
     def send_audio_to_users(self, sock, data):
         """ Sends the audio received to every user """
+
+        # go through ever user
         for user in self.users:
             if user != self.s and user != sock:
                 try:
-                    user.send(data)
+                    user.send(data) # send audio to everyone except yourself
                 except:
                     print("Error sending data to user " + str(user))
-
-
-
-
-
-
-    def user_leave(self, conn):
-        self.users.remove(conn)
-
-    def get_port(self):
-        return self.port
--- server/server.py
+++ server/server.py
@@ -2,30 +2,35 @@ import channel
 import threading
 
 class Server:
+    """ Server that creates two channels that users can connect too
+    Channel 1 Port: 1024, Channel 2 Port: 1025
+    It creates a thread for each channel
+    """
 
-    ip = '54.37.205.19'
-    free_port = 1024         # main channel port = 1024
+    free_port = 1024         # main channel port
 
     def __init__(self):
 
-        # channels
+        # channellist entries: (port, name)
         self.channels = []  # collect channel names
 
-        # create main channel
+        # create channels
         self.create_channel("main")
         self.create_channel("side")
+
+        # message
         print("Server created:")
-        print("   Main Channel (0/10")
-        print("   Side Channel (0/10")
+        print("   Main Channel (0/10)")
+        print("   Side Channel (0/10)")
 
     def create_channel(self, name):
         self.channels.append((name, Server.free_port))
         threading.Thread(target=channel.Channel, args=(name, Server.free_port,)).start()
-        #main_channel = channel.Channel(name, Server.free_port)
         Server.free_port += 1
         print("Channel created: " + str(name))
 
     def delete_channel(self, name, port):
+        """ Remove a channel from the list TODO: disconnect socket """
         self.channels.remove((name, port))
         print("Channel removed: " + name)
 
